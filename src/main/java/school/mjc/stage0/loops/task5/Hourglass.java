cc69f9d710312118a3fca233a98b88dbcbb81809
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-22 17:49:04 +0400

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int k =cathetusLength;
+        for (int i = 0; i < (cathetusLength+1)/2; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < k-2*i; j++) {
+                System.out.print("8");
+            }
+            if(i < (cathetusLength+1)/2-1){
+                System.out.println("");
+            }
+        }
+        for (int i = (cathetusLength+1)/2-1; i >-1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            if (i != (cathetusLength+1)/2-1){
+                for (int j = 0; j < k-2*i; j++) {
+                    System.out.print("8");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
